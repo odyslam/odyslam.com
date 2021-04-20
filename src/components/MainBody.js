c1415d16d3ce05ed89f498465b52aef3f4784e92
odyslam <odyslam@gmail.com>
2021-04-20 08:55:47 +0300

    add mainBody

---
+++ src/components/MainBody.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const mainBody = ({children}) => {
+    return (
+        <div className=" ">
+        <p>another text</p>
+        {children}
+        </div>
+    )    
+}
+
+
+export default mainBody
