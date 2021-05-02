c62ee5f039b8a709a9dbf3b9ce57d4e6f0f672c6
odyslam <odyslam@gmail.com>
2021-05-02 21:02:18 +0300

    change index structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,99 +1,13 @@
 import * as React from "react"
-import Particles from "react-tsparticles";
-import mainBody from "../components/MainBody.js"
+import Layout from '../components/Layout.js'
 
-// markup
+
 const IndexPage = () => {
   return (
     <body>
-      <Particles
-      id="tsparticles"
-      // init={this.particlesInit}
-      // loaded={this.particlesLoaded}
-      className="h-screen absolute w-screen z-0"
-      options={{
-        background: {
-          color: {
-            value: "#0d47a1",
-          },
-        },
-        fpsLimit: 60,
-        interactivity: {
-          detectsOn: "canvas",
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: false,
-            speed: 0.5,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: true,
-      }}/>
-      <main className="flex items-center justify-center h-screen p-10">
-        <div className=" bg-black flex opacity-50 z-10 w-full h-full">
-           
-        </div>
-      </main>
+      <Layout>
+        <p>asfadsfdsfdsf</p>
+      </Layout>
     </body>
   )
 }
