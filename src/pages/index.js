18bc8d260185435dd6bdaf69799dbef4be955e65
odyslam <odyslam@gmail.com>
2021-05-06 07:54:45 +0300

    improve index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import * as React from "react"
 import Layout from '../components/Layout.js'
 
-
 const IndexPage = () => {
   return (
-    <body>
-      <Layout>
-        <p>asfadsfdsfdsf</p>
-      </Layout>
-    </body>
+        <Layout>
+            <main>
+              <p>asfadsfdsfdsf</p>
+            </main>
+        </Layout>
   )
 }
 
