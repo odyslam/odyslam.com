4586763f1e1c66bdf25d3600a17ebd4bd699da02
odyslam <odyslam@gmail.com>
2021-05-06 07:54:34 +0300

    add about mdx and template

---
+++ src/components/MdxPage.js
@@ -0,0 +1,32 @@
+// import * as DesignSystem from "./design-system.js"
+import React from "react"
+import CTA from "./CTA.js"
+import { MDXProvider } from "@mdx-js/react"
+import Layout from './Layout.js'
+import { preToCodeBlock } from "mdx-utils"
+import Code from './CodeBlock.js'
+import Beware from './Beware.js'
+// import * from './design-system.js'
+
+const components = {
+  pre: preProps => {
+    const props = preToCodeBlock(preProps)
+    if (props) {
+      return <Code {...props} />
+    } else {
+      return <pre {...preProps} />
+    }
+  },
+  warning: Beware,
+};
+
+export default ({ children }) => {
+
+  return (
+    <Layout>
+        <MDXProvider components={components}>
+            {children}
+          </MDXProvider>
+    </Layout>
+  )
+}
