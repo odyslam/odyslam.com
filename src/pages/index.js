13ecce15a31b9146596cebc46a4dfec046da1c69
odyslam <odyslam@gmail.com>
2021-05-17 17:03:36 +0300

    add pagequery to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,36 @@
 import * as React from "react"
 import Layout from '../components/Layout.js'
+import { graphql } from "gatsby"
 
-const IndexPage = () => {
+
+const IndexPage = (props) => {
+  console.log(props)
   return (
         <Layout>
             <main>
-              <p>asfadsfdsfdsf</p>
+
             </main>
         </Layout>
   )
 }
 
 export default IndexPage
+
+
+
+export const query = graphql`
+ query SITE_INDEX_QUERY {
+ allMdx(
+ sort: { fields: [frontmatter___date], order: DESC }
+ ) {
+ nodes {
+ id
+ excerpt(pruneLength: 250)
+ frontmatter {
+ title
+ date
+ }
+ }
+ }
+ }
+`
